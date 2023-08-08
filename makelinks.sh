3eb9064de1675d3b536725c1684d6a519c1bc5fd
Carl Zernicke <carlzernicke@outlook.com>
2023-08-07 21:51:22 -0700

    add more and configure waybar

--- makelinks.sh
+++ makelinks.sh
@@ -1,1 +1,11 @@
-ln -s ~/dotfiles/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+ln -sf ~/dotfiles/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
+ln -sf ~/dotfiles/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
+ln -sf ~/dotfiles/polybar/config.ini ~/.config/polybar/config.ini
+ln -sf ~/dotfiles/polybar/heartsclock.sh ~/.config/polybar/heartsclock.sh 
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sf ~/dotfiles/waybar/config ~/.config/waybar/config
+ln -sf ~/dotfiles/waybar/heartsclock-css.sh ~/.config/waybar/heartsclock-css.sh
+ln -sf ~/dotfiles/waybar/style.css ~/.config/waybar/style.css
+ln -sf ~/dotfiles/tofi/config ~/.config/tofi/config
+
