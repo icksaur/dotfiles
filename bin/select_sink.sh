eb45a3071660202c0d0604540f181b31daf42e3c
Carl Zernicke <carlzernicke@outlook.com>
2025-06-09 17:39:26 -0700

    remove all old stuff

--- bin/select_sink.sh
+++
@@ -1,8 +0,0 @@
-device=$(pactl list sinks | grep "Description:" | cut -f 2- -d ' ' | rofi -l 3 -dmenu -p "select an output device:")
-if [ $? -eq 1 ]; then
-  echo "nothing selected, exiting";
-  exit;
-fi
-device_index=$(pactl list sinks | grep -m 1 "$device" -B 3 | head -n 1 | rev | cut -c -1)
-echo "Making sink" $device_index "default."
-pactl set-default-sink $device_index
--- startopenbox.sh
+++
@@ -1,1 +0,0 @@
-sx openbox-session
