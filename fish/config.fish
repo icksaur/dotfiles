1d0fb0ee1c3f46a426d94ac27b773908117d0cc0
Carl Zernicke <carlzernicke@outlook.com>
2026-01-06 15:36:49 -0800

    small updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,8 @@
 set -Ux EDITOR vim
 set -Ux VISUAL vim
+set -Ux NNN_FIFO /tmp/nnn.fifo
+set -Ux NNN_PLUG 'f:fzopen;p:preview-tui'
+set -Ux MAKEFLAGS '-j'(nproc)
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -7,3 +10,5 @@ function nnn
     set -lx NNN_PLUG 'k:preview-tui'
     command nnn -a $argv
 end
+set -U fish_user_paths $HOME/bin $fish_user_paths
+export PATH="$HOME/.local/bin:$PATH"
